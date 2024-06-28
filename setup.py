9bc4e64788b0a576e8027eedbd2db386d20249a0
Ken Seehart <ken@agi.green>
2024-06-28 01:48:14 -0700

    version -> 0.3.1 (fix pypi links)

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='bffl',
-    version='0.3.0',
+    version='0.3.1',
     author='Ken Seehart',
     author_email='ken@agi.green',
     description='A framework for structured bitfield processing',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/bffl',
+    url='https://github.com/kenseehart/bffl',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
