85bff9b5793bb707c10aa5156e5d458afe8f91c8
Ken Seehart <ken@agi.green>
2024-06-28 00:21:18 -0700

    version -> 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bffl',
-    version='0.2.0',
+    version='0.3.0',
     author='Ken Seehart',
     author_email='ken@agi.green',
     description='A framework for structured bitfield processing',
